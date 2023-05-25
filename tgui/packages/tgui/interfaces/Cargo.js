a92e10a7734cdf91ac2e771fdfe1b121d046209c
RKz <falldownftw@gmail.com>
2023-05-24 23:16:14 -0400

    https://github.com/tgstation/tgstation/pull/57298

--- tgui/packages/tgui/interfaces/Cargo.js
+++ tgui/packages/tgui/interfaces/Cargo.js
@@ -73,6 +73,7 @@ export const CargoContent = (props, context) => {
 const CargoStatus = (props, context) => {
   const { act, data } = useBackend(context);
   const {
+    grocery,
     away,
     docked,
     loan,
@@ -96,9 +97,12 @@ const CargoStatus = (props, context) => {
       )}>
       <LabeledList>
         <LabeledList.Item label="Shuttle">
-          {docked && !requestonly && can_send && (
+          {docked && !requestonly && can_send &&(
             <Button
+              color={grocery && "orange" || "green"}
               content={location}
+              tooltip={grocery && "The chef is waiting on their grocery supplies." || ""}
+              tooltipPosition="right"
               onClick={() => act('send')} />
           ) || location}
         </LabeledList.Item>
